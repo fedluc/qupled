8bb5aaf09e6a5f0c48d739414f115606d479667f
Federico Lucco Castello <flc@kth.se>
2022-02-24 14:53:17 +0100

    Added finite temperature calculations for the VS-STLS scheme

--- vs_stls.c
+++ vs_stls.c
@@ -11,7 +11,7 @@
 #include "vs_stls.h"
 
 // -------------------------------------------------------------------
-// FUNCTION USED TO SOLVE THE VS-STLS SCHEME (AT ZERO TEMPERATURE)
+// FUNCTION USED TO SOLVE THE VS-STLS SCHEME
 // -------------------------------------------------------------------
 
 void solve_vs_stls(input in, bool verbose) {
@@ -27,12 +27,6 @@ void solve_vs_stls(input in, bool verbose) {
   // Arrays for VS-STLS solution
   double *uint = NULL;
   double *rsArray = NULL;
-
-  // Check that we are in the ground state
-  if (in.Theta > 0.0) {
-    perror("The VS-STLS is only implemented in the ground state (theta = 0.0)\n");
-    exit(EXIT_FAILURE);
-  }
   
   // Allocate arrays
   alloc_stls_arrays(in, &xx, &phi, &GG, &GG_new, &SS, &SSHF);
@@ -55,7 +49,7 @@ void solve_vs_stls(input in, bool verbose) {
   }
 
   // Iterative procedure to fix the compressibility sum rule
-  if (verbose) printf("Iterative procedure to enforce the CSR rule...\n");
+  if (verbose) printf("Iterative procedure to enforce the compressibility sum rule ...\n");
   in.a_csr = vs_stls_thermo_iterations(xx, uint, rsArray, in, true);
   if (verbose) printf("Done.\n");
 
@@ -65,6 +59,7 @@ void solve_vs_stls(input in, bool verbose) {
   if (verbose) printf("Done.\n");
   
   // Thermodynamic properties
+  if (verbose) printf("Free parameter: %.5f\n",in.a_csr);
   if (verbose) printf("Internal energy: %.10f\n",compute_internal_energy(SS, xx, in));
   if (verbose) printf("Free energy: %.10f\n",compute_free_energy(uint, rsArray, in));
   
@@ -277,24 +272,56 @@ void rs_grid(double *rsArray, input *in){
 
 void compute_vs_slfc(double *GG, double *SS, double *xx, input in) {
 
-  // Arrays for VS-STLS solution
+  bool finite_temperature = false;
   double *GGrs = NULL;
   double *GGrsp = NULL;
   double *GGrsm = NULL;
+  double *GGtp = NULL;
+  double *GGtm = NULL;
   double rs = in.rs;
-
+  double Theta = in.Theta;
+  double der_coeff = 2.0;
+  
+  // Check if finite temperature calculations must be performed
+  if (Theta > 0.0) finite_temperature = true;
+  
   // Allocate arrays
   GGrs = malloc( sizeof(double) * in.nx);
   GGrsp = malloc( sizeof(double) * in.nx);
   GGrsm = malloc( sizeof(double) * in.nx);
+  if (finite_temperature) {
+    GGtp = malloc( sizeof(double) * in.nx);
+    GGtm = malloc( sizeof(double) * in.nx);
+  }
   
-  // STLS local field correction at three state points
+  // STLS local field correction at the state point of interest
   compute_slfc(GGrs, SS, xx, in);
+  
+  // STLS local field correction for coupling parameter (rs) derivative
   in.rs = rs + in.drs;
   compute_slfc(GGrsp, SS, xx, in);
   in.rs = rs - in.drs;
+  if (in.rs <= 0.0) {
+    // Use a first order forward difference approximation for the derivative
+    in.rs = rs;
+    der_coeff = 1.0;
+  }
   compute_slfc(GGrsm, SS, xx, in);
-
+  in.rs = rs;
+  
+  // STLS local field correction for degeneracy parameter (Theta)  derivative
+  // NOTE: For this derivative we use the same resolution used for the rs derivative
+  if (finite_temperature) {
+    if (in.drs >= Theta) {
+      printf("Degeneracy parameter derivative cannot be computed, choose different vs-drs parameter\n");
+      exit(EXIT_FAILURE);
+    }
+    in.Theta = Theta + in.drs;
+    compute_slfc(GGtp, SS, xx, in);
+    in.Theta = Theta - in.drs;
+    compute_slfc(GGtm, SS, xx, in);
+  }
+  
   // VS-STLS static local field correction
   for (int ii=0; ii<in.nx; ii++){
 
@@ -302,12 +329,15 @@ void compute_vs_slfc(double *GG, double *SS, double *xx, input in) {
     GG[ii] = GGrs[ii];
 
     // State point derivative contribution
-    GG[ii] += -in.a_csr/3.0*rs*(GGrsp[ii] - GGrsm[ii])/(2.0*in.drs);
-
+    GG[ii] += -in.a_csr/3.0*rs*(GGrsp[ii] - GGrsm[ii])/(der_coeff*in.drs);
+    if (finite_temperature)
+      GG[ii] += -in.a_csr*2.0/3.0*Theta*(GGtp[ii] - GGtm[ii])/(2.0*in.drs);
+    
     // Wave-vector derivative contribution
     if (ii > 0 && ii < in.nx-1) {
       GG[ii] += -in.a_csr/3.0*xx[ii]*(GGrs[ii+1] - GGrs[ii-1])/(2.0*in.dx);
     }
+
     
   }
   
@@ -315,28 +345,51 @@ void compute_vs_slfc(double *GG, double *SS, double *xx, input in) {
   free(GGrs);
   free(GGrsp);
   free(GGrsm);
+  free(GGtp);
+  free(GGtm);
   
 }
 
 // -------------------------------------------------------------------
-// FUNCTION USED TO COMPUTE THE PARAMETER FOR THE CSR RULE
+// FUNCTION USED TO COMPUTE THE PARAMETER FOR THE COMPRESSIBILITY RULE
 // -------------------------------------------------------------------
 
 double get_alpha(double *xx, double *uint,
 		 double *rsArray, input in) {
 
+  bool finite_temperature = false;
+  double *uinttp = NULL;
+  double *uinttm = NULL;  
   double frs, frsp, frsm;
-  double dudrs;
-  double dfdrs;
-  double d2fdrs2;
+  double ftp, ftm;
+  double frsptp, frsmtp, frsptm, frsmtm;
+  double dudrs, dudt;
+  double dfdrs, dfdt;
+  double d2fdrs2, d2fdt2, d2fdrsdt;
   double numer;
   double denom;
   double alpha;
   double rs = in.rs;
+  double Theta = in.Theta;
+
+  // Check if finite temperature calculations must be performed
+  if (Theta > 0.0) finite_temperature = true;
+  
+  // Allocate temporary arrays for finite temperature calculations
+  if (finite_temperature) {
+    uinttp = malloc( sizeof(double) * in.nrs);
+    uinttm = malloc( sizeof(double) * in.nrs);
+  }
   
   // Get internal energy for thermodynamic integration
   fill_internal_energy_array(xx, uint, rsArray, in, false);
-
+  if (finite_temperature) {
+    in.Theta = Theta + in.drs;
+    fill_internal_energy_array(xx, uinttp, rsArray, in, false);
+    in.Theta = Theta - in.drs;
+    fill_internal_energy_array(xx, uinttm, rsArray, in, false);
+  }
+  
   // Free energy
   frs = compute_free_energy(uint, rsArray, in);
   in.rs = rs + in.drs;
@@ -344,20 +397,48 @@ double get_alpha(double *xx, double *uint,
   in.rs = rs - in.drs;
   frsm = compute_free_energy(uint, rsArray, in);
   in.rs = rs;
+  if (finite_temperature) {
+    ftp = compute_free_energy(uinttp, rsArray, in);
+    ftm = compute_free_energy(uinttm, rsArray, in);
+    in.rs = rs + in.drs;
+    frsptp = compute_free_energy(uinttp, rsArray, in);
+    frsptm = compute_free_energy(uinttm, rsArray, in);
+    in.rs = rs - in.drs;
+    frsmtp = compute_free_energy(uinttp, rsArray, in);
+    frsmtm = compute_free_energy(uinttm, rsArray, in);
+    in.rs = rs;
+  }
   
   // Internal energy derivatives
   dudrs = (uint[in.nrs-1] - uint[in.nrs-3])/(2.0*in.drs);
-
+  if (finite_temperature) 
+    dudt =  (uinttp[in.nrs-2] - uinttm[in.nrs-2])/(2.0*in.drs);
+  
   // Free energy derivatives
   dfdrs = (frsp - frsm)/(2.0*in.drs);
   d2fdrs2 = (frsp -2.0*frs + frsm)/(in.drs*in.drs);
-
-  // Parameter for the CSR rule
-  numer = (2.0*frs - (1.0/6.0)*in.rs*in.rs*d2fdrs2
-	   + (4.0/3.0)*in.rs*dfdrs);
-  denom = (uint[in.nrs-2] + (1.0/3.0)*in.rs*dudrs);
+  if (finite_temperature) {
+    dfdt = (ftp - ftm)/(2.0*in.drs);
+    d2fdt2 = (ftp -2.0*frs + ftm)/(in.drs*in.drs);
+    d2fdrsdt = (frsptp - frsmtp - frsptm + frsmtm)/(4.0*in.drs*in.drs);
+  }
+    
+  // Parameter for the compressibility sum rule
+  numer = (2.0*frs - (1.0/6.0)*rs*rs*d2fdrs2
+	   + (4.0/3.0)*rs*dfdrs);
+  denom = (uint[in.nrs-2] + (1.0/3.0)*rs*dudrs);
+  if (finite_temperature) {
+    numer += -(2.0/3.0)*Theta*Theta*d2fdt2
+             -(2.0/3.0)*Theta*rs*d2fdrsdt
+             +(1.0/3.0)*Theta*dfdt;
+    denom += (2.0/3.0)*Theta*dudt;
+  }
   alpha = numer/denom;
 
+  // Free memory
+  free(uinttp);
+  free(uinttm);
+  
   // Output
   return alpha;
  
