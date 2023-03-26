d834601ac285401fa41447bf6fe03848f18458a3
fedluc <federico.luccocastello@gmail.com>
2023-03-26 17:36:36 +0200

    c++: started implementing python interface

---
+++ python/run.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python
+
+import qupled
+print (qupled.run())
--- src/run.cpp
+++ src/run.cpp
@@ -1,24 +1,25 @@
 #include <omp.h>
 #include <iostream>
+#include <boost/python.hpp>
 #include "input.hpp"
 #include "stls.hpp"
 #include "qstls.hpp"
 
-int main(int argc, char** argv) {
+int run() {
   // Read input
   Input in;
-  if (argc < 2) {
-    cout << "Running with all default values" << endl;
-  }
-  else {
-    try {
-      in.readInput(argv[1]);
-    }
-    catch (const runtime_error& err) {
-      cerr << err.what() << endl;
-      return 1;
-    }
-  }
+  // if (argc < 2) {
+  //   cout << "Running with all default values" << endl;
+  // }
+  // else {
+  //   try {
+  //     in.readInput(argv[1]);
+  //   }
+  //   catch (const runtime_error& err) {
+  //     cerr << err.what() << endl;
+  //     return 1;
+  //   }
+  // }
   // Set number of omp threads
   omp_set_num_threads(in.getNThreads());
   // Compute scheme
@@ -38,4 +39,11 @@ int main(int argc, char** argv) {
   }
   // Return success
   return 0;
+}
+
+
+BOOST_PYTHON_MODULE(qupled)
+{
+    using namespace boost::python;
+    def("run", run);
 }
