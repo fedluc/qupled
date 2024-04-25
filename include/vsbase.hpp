6227cf79ccbf09f5d680f5a50228314daf967fa8
FotisKalk <fokalkavouras@gmail.com>
2024-04-25 15:17:56 +0200

    Fixed indentation

--- include/vsbase.hpp
+++ include/vsbase.hpp
@@ -183,14 +183,14 @@ public:
     if (!fxciData.grid.empty()) {
       for (const auto& theta : {Idx::THETA_DOWN, Idx::THETA, Idx::THETA_UP}) {
         const double rsMaxi = fxciData.grid.back();
-	      const Interpolator1D itp(fxciData.grid, fxciData.integrand[theta]);
-	      for (size_t i = 0; i < nrs; ++i) {
-	        const double& rs = rsGrid[i];
-	        if (rs <= rsMaxi) { fxcIntegrand[theta][i] = itp.eval(rs); }
+        const Interpolator1D itp(fxciData.grid, fxciData.integrand[theta]);
+        for (size_t i = 0; i < nrs; ++i) {
+          const double& rs = rsGrid[i];
+          if (rs <= rsMaxi) { fxcIntegrand[theta][i] = itp.eval(rs); }
           if (theta == Idx::THETA && rs <= rsMaxi) {
             alpha[i] = fxciData.alphaData[i];
           }
-	      }
+        }
       }
     }
   }
