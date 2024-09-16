428838fcb05d89df6a12a69954c5bc55613f0c1f
Federico Lucco Castello <71817492+fedluc@users.noreply.github.com>
2024-09-16 23:39:02 +0200

    Made StlsIet inherit directly from C++

--- python/tests/test_stls_iet.py
+++ python/tests/test_stls_iet.py
@@ -9,7 +9,7 @@ from qupled.classic import Stls
 
 @pytest.fixture
 def stls_iet_instance():
-    return StlsIet(1.0, 1.0, "STLS-HNC")
+    return StlsIet(StlsIet.Input(1.0, 1.0, "STLS-HNC"))
 
 
 def test_default(stls_iet_instance):
@@ -20,61 +20,6 @@ def test_default(stls_iet_instance):
             stls_iet_instance.allowedTheories, ["STLS-HNC", "STLS-IOI", "STLS-LCT"]
         )
     )
-    assert stls_iet_instance.inputs.coupling == 1.0
-    assert stls_iet_instance.inputs.degeneracy == 1.0
-    assert stls_iet_instance.inputs.theory == "STLS-HNC"
-    assert all(
-        x == y
-        for x, y in zip(stls_iet_instance.inputs.chemicalPotential, [-10.0, 10.0])
-    )
-    assert stls_iet_instance.inputs.cutoff == 10.0
-    assert stls_iet_instance.inputs.error == 1.0e-5
-    assert stls_iet_instance.inputs.iet == "standard"
-    assert stls_iet_instance.inputs.mixing == 1.0
-    assert stls_iet_instance.inputs.iterations == 1000
-    assert stls_iet_instance.inputs.matsubara == 128
-    assert stls_iet_instance.inputs.outputFrequency == 10
-    assert stls_iet_instance.inputs.recoveryFile == ""
-    assert stls_iet_instance.inputs.resolution == 0.1
-    assert stls_iet_instance.inputs.int2DScheme == "full"
-    assert stls_iet_instance.inputs.intError == 1.0e-5
-    assert stls_iet_instance.inputs.threads == 1
-    assert stls_iet_instance.scheme is None
-    assert stls_iet_instance.hdfFileName is None
-
-
-def test_set_input():
-    stls_iet = StlsIet(
-        2.0,
-        0.5,
-        "STLS-LCT",
-        [-5, 5],
-        20,
-        1.0e-8,
-        "sqrt",
-        0.5,
-        None,
-        100,
-        32,
-        100,
-        "recoveryFile",
-        0.01,
-        "segregated",
-    )
-    assert stls_iet.inputs.coupling == 2.0
-    assert stls_iet.inputs.degeneracy == 0.5
-    assert stls_iet.inputs.theory == "STLS-LCT"
-    assert all(x == y for x, y in zip(stls_iet.inputs.chemicalPotential, [-5.0, 5.0]))
-    assert stls_iet.inputs.cutoff == 20.0
-    assert stls_iet.inputs.error == 1.0e-8
-    assert stls_iet.inputs.iet == "sqrt"
-    assert stls_iet.inputs.mixing == 0.5
-    assert stls_iet.inputs.iterations == 100
-    assert stls_iet.inputs.matsubara == 32
-    assert stls_iet.inputs.outputFrequency == 100
-    assert stls_iet.inputs.recoveryFile == "recoveryFile"
-    assert stls_iet.inputs.resolution == 0.01
-    assert stls_iet.inputs.int2DScheme == "segregated"
 
 
 def test_compute(stls_iet_instance, mocker):
