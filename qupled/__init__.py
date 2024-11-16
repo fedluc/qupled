dc8ec66e422c16b17a148ab400e624a8d7567028
Federico Lucco Castello <71817492+fedluc@users.noreply.github.com>
2024-11-16 11:43:11 +0100

    Added first version of the files used to build c++ with python

--- qupled/__init__.py
+++ qupled/__init__.py
@@ -1,9 +1,1 @@
-import platform
 
-system = platform.system()
-if system == "Linux":
-    import qupled.Linux.qupled as native
-elif system == "Darwin":
-    import qupled.Darwin.qupled as native
-else:
-    raise ImportError(f"Platform {system} is not supported")
---
+++ setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, Extension
+from setuptools.command.build_ext import build_ext
+import os
+import subprocess
+
+class CMakeBuild(build_ext):
+    def build_extension(self, ext):
+        extdir = os.path.abspath(os.path.dirname(self.get_ext_fullpath(ext.name)))
+        cfg = 'Debug' if self.debug else 'Release'
+        
+        cmake_args = [
+            f"-DCMAKE_LIBRARY_OUTPUT_DIRECTORY={extdir}",
+            f"-DCMAKE_BUILD_TYPE={cfg}",
+        ]
+        
+        build_args = ['--config', cfg]
+        os.makedirs(self.build_temp, exist_ok=True)
+        subprocess.check_call(['cmake', ext.sourcedir] + cmake_args, cwd=self.build_temp)
+        subprocess.check_call(['cmake', '--build', '.'] + build_args, cwd=self.build_temp)
+
+class CMakeExtension(Extension):
+    def __init__(self, name, sourcedir=''):
+        super().__init__(name, sources=[])
+        self.sourcedir = os.path.abspath(sourcedir)
+
+setup(
+    name="qupled",
+    version="0.1.0",
+    description="A package with quantum and classical utilities.",
+    packages=["qupled"],
+    ext_modules=[CMakeExtension("qupled.native.qupled_native", sourcedir="qupled/native/src")],
+    cmdclass={"build_ext": CMakeBuild},
+    zip_safe=False,
+)
