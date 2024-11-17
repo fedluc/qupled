faa52cea0c48d25e77225e9a5a79f22967effa78
Federico Lucco Castello <71817492+fedluc@users.noreply.github.com>
2024-11-17 01:05:42 +0100

    Added dedicated python script to build, test, write docs and format

--- setup.py
+++ setup.py
@@ -3,25 +3,32 @@ from setuptools.command.build_ext import build_ext
 import os
 import subprocess
 
+
 class CMakeBuild(build_ext):
     def build_extension(self, ext):
         extdir = os.path.abspath(os.path.dirname(self.get_ext_fullpath(ext.name)))
-        cfg = 'Debug' if self.debug else 'Release'
-        
+        cfg = "Debug" if self.debug else "Release"
+
         cmake_args = [
             f"-DCMAKE_LIBRARY_OUTPUT_DIRECTORY={extdir}",
             f"-DCMAKE_BUILD_TYPE={cfg}",
         ]
-        
-        build_args = ['--config', cfg]
+
+        build_args = ["--config", cfg]
         os.makedirs(self.build_temp, exist_ok=True)
-        subprocess.check_call(['cmake', ext.sourcedir] + cmake_args, cwd=self.build_temp)
-        subprocess.check_call(['cmake', '--build', '.'] + build_args, cwd=self.build_temp)
+        subprocess.check_call(
+            ["cmake", ext.sourcedir] + cmake_args, cwd=self.build_temp
+        )
+        subprocess.check_call(
+            ["cmake", "--build", "."] + build_args, cwd=self.build_temp
+        )
+
 
 class CMakeExtension(Extension):
-    def __init__(self, name, sourcedir=''):
+    def __init__(self, name, sourcedir=""):
         super().__init__(name, sources=[])
         self.sourcedir = os.path.abspath(sourcedir)
+
 
 setup(
     # name="qupled",
