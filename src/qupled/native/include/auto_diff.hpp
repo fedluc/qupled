f261e3818b64ab5d81e11d5f9a0f85247df0d121
Federico Lucco Castello <71817492+fedluc@users.noreply.github.com>
2024-12-01 21:05:10 +0100

    Revert "Refactored operator overloads"
    This reverts commit b9a60d16eec9d04613fb31023368b5d2eebc50ef.

--- src/qupled/native/include/auto_diff.hpp
+++ src/qupled/native/include/auto_diff.hpp
@@ -28,16 +28,20 @@ public:
 // addition operators
 template <typename T>
 AutoDiff<T> operator+(const AutoDiff<T> &dual1, const AutoDiff<T> &dual2) {
-  std::vector<T> der(NVAR);
+  AutoDiff<T> result = AutoDiff<T>(dual1.val + dual2.val);
   for (size_t i = 0; i < NVAR; ++i) {
-    der[i] = dual1.der[i] + dual2.der[i];
+    result.der[i] = dual1.der[i] + dual2.der[i];
   }
-  return AutoDiff<T>(dual1.val + dual2.val, der);
+  return result;
 }
 
 template <typename T>
 AutoDiff<T> operator+(const AutoDiff<T> &dual, const double &scalar) {
-  return AutoDiff<T>(dual.val + scalar, dual.der);
+  AutoDiff<T> result = AutoDiff<T>(dual.val + scalar);
+  for (size_t i = 0; i < NVAR; ++i) {
+    result.der[i] = dual.der[i];
+  }
+  return result;
 }
 
 template <typename T>
@@ -48,44 +52,48 @@ AutoDiff<T> operator+(const double &scalar, const AutoDiff<T> &dual) {
 // subtraction operators
 template <typename T>
 AutoDiff<T> operator-(const AutoDiff<T> &dual1, const AutoDiff<T> &dual2) {
-  std::vector<T> der(NVAR);
+  AutoDiff<T> result = AutoDiff<T>(dual1.val - dual2.val);
   for (size_t i = 0; i < NVAR; ++i) {
-    der[i] = dual1.der[i] - dual2.der[i];
+    result.der[i] = dual1.der[i] - dual2.der[i];
   }
-  return AutoDiff<T>(dual1.val - dual2.val, der);
+  return result;
 }
 
 template <typename T>
 AutoDiff<T> operator-(const AutoDiff<T> &dual, double scalar) {
-  return AutoDiff<T>(dual.val - scalar, dual.der);
+  AutoDiff<T> result = AutoDiff<T>(dual.val - scalar);
+  for (size_t i = 0; i < NVAR; ++i) {
+    result.der[i] = dual.der[i];
+  }
+  return result;
 }
 
 template <typename T>
 AutoDiff<T> operator-(const double &scalar, const AutoDiff<T> &dual) {
-  std::vector<T> der(NVAR);
+  AutoDiff<T> result = AutoDiff<T>(scalar - dual.val);
   for (size_t i = 0; i < NVAR; ++i) {
-    der[i] = -dual.der[i];
+    result.der[i] = -dual.der[i];
   }
-  return AutoDiff<T>(scalar - dual.val, der);
+  return result;
 }
 
 // multiplication operators
 template <typename T>
 AutoDiff<T> operator*(const AutoDiff<T> &dual1, const AutoDiff<T> &dual2) {
-  std::vector<T> der(NVAR);
+  AutoDiff<T> result = AutoDiff<T>(dual1.val * dual2.val);
   for (size_t i = 0; i < NVAR; ++i) {
-    der[i] = dual1.val * dual2.der[i] + dual1.der[i] * dual2.val;
+    result.der[i] = dual1.val * dual2.der[i] + dual1.der[i] * dual2.val;
   }
-  return AutoDiff<T>(dual1.val * dual2.val, der);
+  return result;
 }
 
 template <typename T>
 AutoDiff<T> operator*(const AutoDiff<T> &dual, const double &scalar) {
-  std::vector<T> der(NVAR);
+  AutoDiff<T> result = AutoDiff<T>(dual.val * scalar);
   for (size_t i = 0; i < NVAR; ++i) {
-    der[i] = dual.der[i] * scalar;
+    result.der[i] = dual.der[i] * scalar;
   }
-  return AutoDiff<T>(dual.val * scalar, der);
+  return result;
 }
 
 template <typename T>
@@ -97,43 +105,44 @@ AutoDiff<T> operator*(const double &scalar, const AutoDiff<T> &dual) {
 template <typename T>
 AutoDiff<T> operator/(const AutoDiff<T> &dual1, const AutoDiff<T> &dual2) {
   const double inv_val = 1.0 / dual2.val;
-  std::vector<T> der(NVAR);
+  AutoDiff<T> result = AutoDiff<T>(dual1.val * inv_val);
   for (size_t i = 0; i < NVAR; ++i) {
-    der[i] = (dual1.der[i] - dual1.val * dual2.der[i] * inv_val) * inv_val;
+    result.der[i] =
+        (dual1.der[i] - dual1.val * dual2.der[i] * inv_val) * inv_val;
   }
-  return AutoDiff<T>(dual1.val * inv_val, der);
+  return result;
 }
 
 template <typename T>
 AutoDiff<T> operator/(const AutoDiff<T> &dual, const double &scalar) {
   const double inv_scalar = 1.0 / scalar;
-  std::vector<T> der(NVAR);
+  AutoDiff<T> result = AutoDiff<T>(dual.val * inv_scalar);
   for (size_t i = 0; i < NVAR; ++i) {
-    der[i] = dual.der[i] * inv_scalar;
+    result.der[i] = dual.der[i] * inv_scalar;
   }
-  return AutoDiff<T>(dual.val * inv_scalar, der);
+  return result;
 }
 
 template <typename T>
 AutoDiff<T> operator/(const double &scalar, const AutoDiff<T> &dual) {
   const double inv_val = 1.0 / dual.val;
   const double inv_val2 = inv_val * inv_val;
-  std::vector<T> der(NVAR);
+  AutoDiff<T> result = AutoDiff<T>(scalar * inv_val);
   for (size_t i = 0; i < NVAR; ++i) {
-    der[i] = -scalar * dual.der[i] * inv_val2;
+    result.der[i] = -scalar * dual.der[i] * inv_val2;
   }
-  return AutoDiff<T>(scalar * inv_val, der);
+  return result;
 }
 
 // exponential function
 template <typename T>
 AutoDiff<T> exp(const AutoDiff<T> &x) {
   const double exp_val = exp(x.val);
-  std::vector<T> der(NVAR);
+  AutoDiff<T> result = AutoDiff<T>(exp_val);
   for (size_t i = 0; i < NVAR; ++i) {
-    der[i] = exp_val * x.der[i];
+    result.der[i] = exp_val * x.der[i];
   }
-  return AutoDiff<T>(exp_val, der);
+  return result;
 }
 
 // square root function
@@ -141,11 +150,11 @@ template <typename T>
 AutoDiff<T> sqrt(const AutoDiff<T> &x) {
   const double sqrt_val = sqrt(x.val);
   const double inv_sqrt = 0.5 / sqrt_val;
-  std::vector<T> der(NVAR);
+  AutoDiff<T> result = AutoDiff<T>(sqrt_val);
   for (size_t i = 0; i < NVAR; ++i) {
-    der[i] = x.der[i] * inv_sqrt;
+    result.der[i] = x.der[i] * inv_sqrt;
   }
-  return AutoDiff<T>(sqrt_val, der);
+  return result;
 }
 
 // hyperbolic tangent function
@@ -153,11 +162,11 @@ template <typename T>
 AutoDiff<T> tanh(const AutoDiff<T> &x) {
   const double tanh_val = tanh(x.val);
   const double sech2_val = 1.0 - tanh_val * tanh_val;
-  std::vector<T> der(NVAR);
+  AutoDiff<T> result = AutoDiff<T>(tanh_val);
   for (size_t i = 0; i < NVAR; ++i) {
-    der[i] = x.der[i] * sech2_val;
+    result.der[i] = x.der[i] * sech2_val;
   }
-  return AutoDiff<T>(tanh_val, der);
+  return result;
 }
 
 // First order derivatives
@@ -172,8 +181,8 @@ public:
   AutoDiff1(const AutoDiff<double> &other)
       : AutoDiff<double>(other) {}
   // Aliases for convenient access of the results
-  std::reference_wrapper<double> dx = der[0];
-  std::reference_wrapper<double> dy = der[1];
+  double dx = der[0];
+  double dy = der[1];
 };
 
 // Second order derivatives
@@ -188,11 +197,11 @@ public:
   AutoDiff2(const AutoDiff<AutoDiff1> &other)
       : AutoDiff<AutoDiff1>(other) {}
   // Aliases for convenient access of the results
-  std::reference_wrapper<double> dx = der[0].val;
-  std::reference_wrapper<double> dy = der[1].val;
-  std::reference_wrapper<double> dxx = der[0].der[0];
-  std::reference_wrapper<double> dxy = der[0].der[1];
-  std::reference_wrapper<double> dyy = der[1].der[1];
+  double dx = der[0].val;
+  double dy = der[1].val;
+  double dxx = der[0].der[0];
+  double dxy = der[0].der[1];
+  double dyy = der[1].der[1];
 };
 
 #endif
