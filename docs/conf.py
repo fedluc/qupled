fab04d7f415b1c4898d9c80637555b1f4e20eca3
Federico Lucco Castello <71817492+fedluc@users.noreply.github.com>
2024-12-06 16:30:05 +0100

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -14,7 +14,7 @@ author = "Federico Lucco Castello"
 import os
 import sys
 
-sys.path.insert(0, os.path.abspath(os.path.join("..","src"))
+sys.path.insert(0, os.path.abspath(os.path.join("..","src")))
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
