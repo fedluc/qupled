037c645128fb50f813dd8787b60ff830802e970b
Federico Lucco Castello <71817492+fedluc@users.noreply.github.com>
2024-12-09 16:43:09 +0100

    Created script for dependency installation

---
+++ dev/install_external_dependencies.py
@@ -0,0 +1,42 @@
+import os
+import platform
+import subprocess
+
+
+def install_dependencies():
+    system = platform.system()
+    if system == "Linux":
+        subprocess.check_call(["apt-get", "update"])
+        subprocess.check_call(
+            [
+                "apt-get",
+                "install",
+                "-y",
+                "cmake",
+                "libboost-all-dev",
+                "libopenmpi-dev",
+                "libgsl-dev",
+                "libomp-dev",
+                "libfmt-dev",
+                "python3-dev",
+            ]
+        )
+    elif system == "Darwin":
+        subprocess.check_call(
+            [
+                "brew",
+                "install",
+                "cmake",
+                "gsl",
+                "libomp",
+                "openmpi",
+                "fmt",
+                "boost-python3",
+            ]
+        )
+    else:
+        raise RuntimeError(f"Unsupported platform: {system}")
+
+
+if __name__ == "__main__":
+    install_dependencies()
