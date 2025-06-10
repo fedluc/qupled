eae4f097e5f5988704a12e6568dd400700fcc9f6
Federico Lucco Castello <71817492+fedluc@users.noreply.github.com>
2025-06-10 22:12:07 +0200

    Fixed broken test

--- tests/test_mpi.py
+++ tests/test_mpi.py
@@ -9,13 +9,13 @@ def mpi_native(mocker):
 
 
 def test_mpi_rank(mpi_native):
-    mpi_native.return_value.rank.return_value = 0
+    mpi_native.rank.return_value = 0
     mpi = MPI()
     assert mpi.rank() == 0
 
 
 def test_mpi_is_root(mpi_native):
-    mpi_native.return_value.is_root.return_value = True
+    mpi_native.is_root.return_value = True
     mpi = MPI()
     assert mpi.is_root() is True
 
@@ -23,17 +23,17 @@ def test_mpi_is_root(mpi_native):
 def test_mpi_barrier(mpi_native):
     mpi = MPI()
     mpi.barrier()
-    mpi_native.return_value.barrier.assert_called_once()
+    mpi_native.barrier.assert_called_once()
 
 
 def test_mpi_timer(mpi_native):
-    mpi_native.return_value.timer.return_value = 123.456
+    mpi_native.timer.return_value = 123.456
     mpi = MPI()
     assert mpi.timer() == 123.456
 
 
 def test_run_only_on_root_decorator(mpi_native):
-    mpi_native.return_value.is_root.return_value = True
+    mpi_native.is_root.return_value = True
 
     @MPI.run_only_on_root
     def test_func():
@@ -48,11 +48,11 @@ def test_synchronize_ranks_decorator(mpi_native):
         pass
 
     test_func()
-    mpi_native.return_value.barrier.assert_called_once()
+    mpi_native.barrier.assert_called_once()
 
 
 def test_record_time_decorator(mpi_native, mocker):
-    mpi_native.return_value.timer.side_effect = [0, 3600]
+    mpi_native.timer.side_effect = [0, 3600]
 
     @MPI.record_time
     def test_func():
