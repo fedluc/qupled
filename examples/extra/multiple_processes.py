1c5d84859c7b02a09ad947a9133f4e33c44be267
Federico Lucco Castello <71817492+fedluc@users.noreply.github.com>
2025-10-09 00:48:15 +0200

    Update docs (#66)
    * Updated docs
    
    * added test and update dimension docstring
    
    * Added safeguards for 2D calculations and new dimension enumerator in Python
    
    * Added Dimension enumerator to the docs
    
    * Fixed formatting

--- examples/extra/multiple_processes.py
+++ examples/extra/multiple_processes.py
@@ -1,11 +1,12 @@
 import qupled.stls as stls
 from qupled.output import DataBase
+from qupled.dimension import Dimension
 
 # Define the object used to solve the scheme
 scheme = stls.Solver()
 
 # Define the input parameters
-inputs = stls.Input(10.0, 1.0, mixing=0.5)
+inputs = stls.Input(10.0, 1.0, mixing=0.2, processes=2, dimension=Dimension._3D)
 
 # Solve scheme
 scheme.compute(inputs)
